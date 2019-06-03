b5e15ebfd96cd779ac1d1334a142978deca90752
MikeQin <mike.qin@gmail.com>
2019-06-03 01:41:26 -0400

    Init

--- server.js
+++ server.js
@@ -1,43 +1,12 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
-const { buildSchema } = require('graphql');
-const bookSchema = require('./schema/schema');
-
-const userSchema = buildSchema(`
-type User {
-  id: String
-  name: String
-}
-
-type Query {
-  user(id: String): User
-}
-`);
-
-// Maps id to User object
-var fakeDatabase = {
-  'a': {
-    id: 'a',
-    name: 'alice',
-  },
-  'b': {
-    id: 'b',
-    name: 'bob',
-  }
-};
-
-var root = {
-  user: function ({ id }) {
-    return fakeDatabase[id];
-  }
-};
-
+const bookSchema = require('./schema/bookSchema');
 const app = express();
+
 app.use(
   "/graphql",
   graphqlHTTP({
     schema: bookSchema,
-    //rootValue: root,
     graphiql: true,
     pretty: true
   })
---
+++ user.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const { buildSchema } = require('graphql');
+
+const userSchema = buildSchema(`
+type User {
+  id: String
+  name: String
+}
+
+type Query {
+  user(id: String): User
+}
+`);
+
+// Maps id to User object
+var fakeDatabase = {
+  'a': {
+    id: 'a',
+    name: 'alice',
+  },
+  'b': {
+    id: 'b',
+    name: 'bob',
+  }
+};
+
+var root = {
+  user: function ({ id }) {
+    return fakeDatabase[id];
+  }
+};
+
+const app = express();
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: userSchema,
+    rootValue: root,
+    graphiql: true,
+    pretty: true
+  })
+);
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`Server listens on port ${port} ...`));
